b5216b1ec48643084c2d0240a4ed0e6ce66c7094
andyzhu7568 <andyzhu7568@gmail.com>
2021-12-05 00:48:42 -0700

    fixed merged conflict error

--- launchpad/src/App.js
+++ launchpad/src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import PostalLookUp from './pages/PostalLookup';
-import Universities from './pages/Universities';
-import Header from './components/Header'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import PostalLookUp from "./pages/PostalLookup";
+import Universities from "./pages/Universities";
+import Header from "./components/Header";
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route exact path='/posts/*' element={<Home />} />
-        <Route exact path='/postallookup' element={<PostalLookUp />} 
-        <Route exact path='/universities' element={<Universities />} />
-        <Route path='*' element={<Home />} />
+        <Route exact path="/posts/*" element={<Home />} />
+        <Route exact path="/postallookup" element={<PostalLookUp />} />
+        <Route exact path="/universities" element={<Universities />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
